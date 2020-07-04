faf12bdd8cb690deb6751168bde7164b18a0bc0c
Sham <shambhawi.kumari1393@gmail.com>
2020-07-04 10:23:44 -0700

    added files for testing

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var rapidAPIKey = 'c6064b5170msh977e24b1ae6c804p17fd8djsnf539fedd1d13';
+var resultObject;
+
+//converts image to base 64
+function readFile() {
+    if (this.files && this.files[0]) {
+        $('#uploadedFileName').removeClass('hide');
+        $('#uploadedFileName').text(this.files[0].name);
+        var FR = new FileReader();
+        $(FR).on("load", function (e) {
+            console.log('b64 result: ' + e.target.result);
+            b64 = e.target.result;
+
+        });
+        FR.readAsDataURL(this.files[0]);
+    }
+}
+
+$("#fileInput").on("change", readFile);
+
+function submit(event){
+    //event.preventDefault();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://google-ai-vision.p.rapidapi.com/cloudVision/imageObjectDetection",
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "google-ai-vision.p.rapidapi.com",
+            "x-rapidapi-key": rapidAPIKey,
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        "processData": false,
+        "data": "{ \"source\":\"" + b64 + "\", \"sourceType\": \"base64\"}"
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        localStorage.setItem('scanned-image', JSON.stringify(response.objects));
+        resultObject = _.map(response.objects, 'name');
+        resultObject = _.uniq(resultObject);
+    });
+}
+
